72d01e837d16dd79a089d7db7e8589de64dfccf7
mmullerx4 <mmullerx4@gmail.com>
2024-05-07 19:46:38 -0400

    finished assign 2

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,23 +1,27 @@
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
+
 let product = {};
 
-export default async function productDetails(productId) {
-
+export default async function productDetails(productId) { product = await findProductById(productId);
+  renderProductDetails();
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
-function addToCart(product) {
+function addToCart() {
     setLocalStorage("so-cart", product);
   }
-  // add to cart button event handler
-  async function addToCartHandler(e) {
-    const product = await findProductById(e.target.dataset.id);
-    addToCart(product);
+
+  function renderProductDetails() {
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText =
+      product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText =
+      product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+      product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
   }
-
-
- 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  renderProductDetails()
