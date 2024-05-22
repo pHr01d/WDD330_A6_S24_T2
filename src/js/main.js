0f41e3cb95fbb073a4009dd0d1818b50b39dee46
pHr01d <pro20006@byui.edu>
2024-05-21 19:23:52 -0500

    Finish Team4 activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-import {productList} from "./productList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import productList from "./productList.mjs";
+
+loadHeaderFooter();
+productList(".product-list", "tents");
