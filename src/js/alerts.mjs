445026dcb74d10162150d4266af752f45fb73ae4
mmullerx4 <mmullerx4@gmail.com>
2024-05-27 19:50:05 -0400

    alert to index

---
+++ src/js/alerts.mjs
@@ -0,0 +1,23 @@
+import { getData } from "./productData.mjs";
+import { renderListWithTemplate, qs } from "./utils.mjs";
+
+
+//1.needs to read the alerts.json and if finds alert should create a <section>  with the class alert-list
+//2.loop through list (json entries) and create <p> for each alert
+//3.And apply background and foreground colors to the alert
+//4.Then the <section> should be prepended to the <main> on the index
+
+
+function alertsTemplate(alertList) {
+  return `<p style="background-color:${alertList.background}; color:${alertList.color}">${alertList.message}</p>`
+  }
+
+  export default async function alertsList() {
+    // get the element we will insert the list into from the selector
+    const el = qs(".alerts-list");
+    const alerts = await getData("alerts");
+  
+    // render out the product list to the element
+    renderListWithTemplate(alertsTemplate, el, alerts);
+  }
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import productList from "./productList.mjs";
+import alertsList from "./alerts.mjs";
 
 loadHeaderFooter();
+alertsList();
 productList(".product-list", "tents");
+
