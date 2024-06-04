ea246094aad7707625ab2da1262463f8fae9a6a6
pHr01d <pro20006@byui.edu>
2024-06-03 21:41:30 -0500

    minor fixes

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate, qs } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-  <a href="/product-pages/index.html?product=${product.Id}">
+  <a href="/product_pages/index.html?product=${product.Id}">
   <img
     src="${product.Images.PrimaryMedium}"
     alt="Image of ${product.Name}"
