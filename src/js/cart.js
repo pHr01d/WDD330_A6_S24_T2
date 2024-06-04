da840830735672c2acaaf133a4d73a5ae157d4e9
pHr01d <pro20006@byui.edu>
2024-06-03 21:25:01 -0500

    Added cart total, fixed product page display errors, css cart page, deleted unused files

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ShoppingCart from "./shoppingCart.mjs";
+import shoppingCart from "./shoppingCart.mjs";
 
 loadHeaderFooter();
-ShoppingCart();
+shoppingCart();
 
--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -1,16 +1,35 @@
 import { getLocalStorage, renderListWithTemplate, qs } from "./utils.mjs";
 
-export default function ShoppingCart () {
+export default function shoppingCart () {
   const cartItems = getLocalStorage("so-cart");
-  const el = qs(".product-list");
+  const el = qs(".cart-list");
   renderListWithTemplate(cartItemTemplate, el, cartItems);
+  const cartTotal = calcListTotal(cartItems);
+  displayCartTotal(cartTotal);
+}
+
+function displayCartTotal(total) {
+  if (total > 0) {
+    // If the total is greater than zero then show the total
+    qs(".cart-footer").classList.remove("hide");
+    qs(".cart-total").innerText += ` $${total}`
+  } else {
+    // otherwise, hide the total
+    qs(".cart-footer").classList.add("hide");
+  }
+}
+
+function calcListTotal(list) {
+  const amounts = list.map((item) => item.FinalPrice);
+  const total = amounts.reduce((sum,item) => sum + item, 0);
+  return total;
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  const cartItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -18,9 +37,8 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">Qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-
-  return newItem;
+  return cartItem;
 }
